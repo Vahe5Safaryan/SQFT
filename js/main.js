1550c9cf2d931f035a1b8e37c27f2e4395e880cc
Vahe <vsafaryan50@gmail.com>
2023-05-25 06:09:07 -0700

    SelectorLanguage

--- js/main.js
+++ js/main.js
@@ -1581,3 +1581,38 @@ $('.modal-slider.slick-slider').on('afterChange', function (event, slick, curren
         });
     }
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function(){
+//
+//     let multipleCancelButton = new Choices('#choices-multiple-remove-button', {
+//         removeItemButton: true,
+//         maxItemCount:5,
+//         searchResultLimit:5,
+//         renderChoiceLimit:5
+//     });
+//
+// });
+
