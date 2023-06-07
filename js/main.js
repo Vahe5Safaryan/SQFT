364218935a73b3e4e5fa103021901fd9ac0ce7a6
Vahe <vsafaryan50@gmail.com>
2023-06-08 04:18:22 +1400

    update git

--- js/main.js
+++ js/main.js
@@ -107,8 +107,6 @@ $('.partner-section').slick({
 //  Partner End //
 
 
-
-
 // // Payment Section //
 $('.plan-monthly').slick({
     dots: false,
@@ -1866,3 +1864,56 @@ heartIcons.forEach(function(icon) {
         }
     });
 });
+
+
+
+//  Modal Script Start //
+function CustomAlert(){
+    this.alert = function(message,title){
+        document.body.innerHTML = document.body.innerHTML + '<div id="dialogoverlay"></div><div id="dialogbox" class="slit-in-vertical"><div><div id="dialogboxhead"></div><div id="dialogboxbody"></div><div id="dialogboxfoot"></div></div></div>';
+
+        let dialogoverlay = document.getElementById('dialogoverlay');
+        let dialogbox = document.getElementById('dialogbox');
+
+        let winH = window.innerHeight;
+        dialogoverlay.style.height = winH+"px";
+
+        dialogbox.style.top = "100px";
+
+        dialogoverlay.style.display = "block";
+        dialogbox.style.display = "block";
+
+        document.getElementById('dialogboxhead').style.display = 'block';
+
+        if(typeof title === 'undefined') {
+            document.getElementById('dialogboxhead').style.display = 'none';
+        } else {
+            document.getElementById('dialogboxhead').innerHTML = '<i class="fa fa-exclamation-circle" aria-hidden="true"></i> '+ title;
+        }
+        document.getElementById('dialogboxbody').innerHTML = message;
+        document.getElementById('dialogboxfoot').innerHTML = '<button class="pure-material-button-contained active" onclick="customAlert.ok()">OK</button>';
+    }
+
+    this.ok = function(){
+        document.getElementById('dialogbox').style.display = "none";
+        document.getElementById('dialogoverlay').style.display = "none";
+    }
+}
+let customAlert = new CustomAlert();
+//  Modal Script End //
+
+
+//  Scroll Start //
+document.addEventListener('DOMContentLoaded', function() {
+    let navLinks = document.querySelectorAll('.nav-link');
+    for (let i = 0; i < navLinks.length; i++) {
+        navLinks[i].addEventListener('click', function(e) {
+            e.preventDefault();
+
+            let targetSectionId = this.getAttribute('href').substring(1);
+            let targetSection = document.getElementById(targetSectionId);
+            targetSection.scrollIntoView({ behavior: 'smooth' });
+        });
+    }
+});
+//  Scroll End //
